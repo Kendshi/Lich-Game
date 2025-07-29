20c17544b40d4b82937d3b68f1d59bb9e28380fc
Kendshi <gamayunovantonsakh@gmail.com>
2025-07-29 11:02:50 +0300

    Добавил скрипты Squad и Unit

--- Assets/Assets/Scripts/Squad.cs
+++ Assets/Assets/Scripts/Squad.cs
@@ -1,16 +1,35 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Squad : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] List<Transform> positionPoints = new List<Transform>();
+
+    private List<Unit> _units = new List<Unit>();
+    private const int MAX_UNITS_IN_SQUAD = 9;
+
     void Start()
+    {
+        foreach (Unit unit in _units) 
+        {
+            Instantiate(unit);
+        }
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GetSpawnUnits(List<Unit> units)
     {
-        
+        if (units.Count > MAX_UNITS_IN_SQUAD)
+        {
+            Debug.LogError("Количество юнитов превышает максимальный лимит отряда");
+            return;
+        }
+
+        _units = units;
     }
 }
---
+++ Assets/Assets/Scripts/Unit.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public abstract class Unit : MonoBehaviour
+{
+    public int SquadPosition;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
